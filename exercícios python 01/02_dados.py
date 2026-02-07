2d79cadc524a9fd86b62c40b6cc0cacf3ba5d28a
Kelly <kellyvnss00@gmail.com>
2026-02-06 22:11:36 -0300

    Exercícios 1, 2 e 3

---
+++ exercícios python 01/02_dados.py
@@ -0,0 +1,5 @@
+nome = input ('Informe seu nome: ')
+idade = int (input ('Informe sua idade: '))
+altura = float (input ('Informe sua altura: '))
+
+print ('Olá ', nome, ', você tem ', idade, 'anos e mede ', altura, ' cm de altura')
---
+++ exercícios python 01/03_calculos.py
@@ -0,0 +1,12 @@
+a = int(input('Digite o valor de a: '))
+b = int(input('Digite o valor de b: '))
+
+soma = (a + b)
+subtracao = (a - b)
+multiplicacao = (a * b)
+divisao = (a / b)
+
+print('A soma de ',a, '+',b, 'é: ', soma)
+print('A subtração de ',a, '-',b, 'é: ', subtracao)
+print('A multiplicação de ',a, '*',b, 'é: ', multiplicacao)
+print('A divisao de ',a, '%',b, 'é: ', divisao)
